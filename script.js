c4ebabe6788d5740fdf041031f3440ac0b3943ed
ryan hatfield <hatfield086@gmail.com>
2020-08-08 21:02:45 -0500

    added the remaining prompts

--- script.js
+++ script.js
@@ -18,7 +18,38 @@ function writePassword() {
   // Prompt for password length.
   var passLength = (prompt("Please enter the amount of characters your would like, must be between 8 and 128."));
   console.log(passLength);
- 
+
+//Prompt to add uppercase letters. 
+  upperCase = confirm("Do you want to include uppercase? Click OK for yes and Cancel for no.");
+  if (upperCase === true) {
+    criteria = criteria.concat("uppercase");
+    console.log("uppercase");
+    criteriaChoices = criteriaChoices.concat(upper);
+  }
+
+//Prompt to add uppercase letters. 
+  lowerCase = confirm("Do you want to include lowercase? Click OK for yes and Cancel for no.");
+  if (lowerCase === true) {
+    criteria = criteria.concat("lowercase");
+    console.log("lowercase");
+    criteriaChoices = criteriaChoices.concat(lower);
+  }
+
+//Prompt to add numbers. 
+  numeric = confirm("Do you want to include numbers? Click OK for yes and Cancel for no.");
+  if (numeric === true) {
+    criteria = criteria.concat("numbers");
+    console.log("numbers");
+    criteriaChoices = criteriaChoices.concat(number);
+  }
+
+//Prompt to add spcecial characters. 
+  specialCh = confirm("Do you want to include special characters? Click OK for yes and Cancel for no.");
+  if (specialCh === true) {
+    criteria = criteria.concat("special characters");
+    console.log("special characters");
+    criteriaChoices = criteriaChoices.concat(special);
+  }
 
 
   var password = generatePassword();
